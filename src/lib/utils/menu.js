82175eb26080b8c382ea3257de292ce6260e535e
Mehul <fiveonline.in@gmail.com>
2026-03-11 13:14:15 +0530

    feat: add navigation menu items for common, admin, trainer, and candidate roles.

--- src/lib/utils/menu.js
+++ src/lib/utils/menu.js
@@ -69,7 +69,7 @@ export const MenuItems = [
     permissionSlug: "view_master_courses",
     subItems: [
       { title: "Master Courses", url: "/courses" },
-      { title: "Pre-Active Courses", url: "/pre-active-courses" },
+      // { title: "Pre-Active Courses", url: "/pre-active-courses" },
       // { title: "Active Courses", url: "/active-courses" },
     ],
   },
